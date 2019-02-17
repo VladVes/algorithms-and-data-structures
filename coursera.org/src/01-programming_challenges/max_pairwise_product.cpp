34fb85fae40aa1af6682767dd17e10cdce6ab9cd
VladVes <veselov.workflow@gmail.com>
2019-02-17 21:24:30 +0700

    update max pairwies product with stress test

--- coursera.org/src/01-programming_challenges/max_pairwise_product.cpp
+++ coursera.org/src/01-programming_challenges/max_pairwise_product.cpp
@@ -2,6 +2,10 @@
 #include <vector>
 #include <algorithm>
 
+using std::vector;
+using std::cin;
+using std::cout;
+
 int MaxPairwiseProduct(const std::vector<int>& numbers) {
     int max_product = 0;
     int n = numbers.size();
@@ -16,14 +20,55 @@ int MaxPairwiseProduct(const std::vector<int>& numbers) {
     return max_product;
 }
 
+int MaxPairwiseProductFast(const vector<int>& numbers) {
+    int n = numbers.size();
+    
+    int max_index1 = -1;
+    for (int i = 0; i < n; ++i)
+        if ((max_index1 == -1) || (numbers[i] > numbers[max_index1]))
+    	   max_index1 = i;		
+    
+    int max_index2 = -1;
+    for (int j = 0; j < n; ++j)
+    	if ((j != max_index1) && ((max_index2 == -1) || (numbers[j] > numbers[max_index2])))
+	       max_index2 = j;
+
+    cout << max_index1 << " " << max_index2 << "\n";
+
+    return ((long long)(numbers[max_index1])) * numbers[max_index2];
+}
+
+
 int main() {
+    while (true) {
+        int n = rand() % 100 + 2;
+        cout << n << "\n";
+        vector<int> a;
+        for (int i = 0; i < n; ++i) {
+            a.push_back(rand() % 100000);
+        }    
+        for (int i = 0; i < n; ++i) {
+            cout << a[i] << ' ';
+        }
+        cout << "\n";
+        long long res1 = MaxPairwiseProduct(a);
+        long long res2 = MaxPairwiseProductFast(a);
+        if (res1 != res2) {
+            cout << "Wrong answer: " << res1 << " " << res2 << "\n";
+            break;
+        }
+        else {
+            cout << "OK\n";
+        }
+    } 
     int n;
-    std::cin >> n;
-    std::vector<int> numbers(n);
+    cin >> n;
+    vector<int> numbers(n);
     for (int i = 0; i < n; ++i) {
-        std::cin >> numbers[i];
+        cin >> numbers[i];
     }
 
-    std::cout << MaxPairwiseProduct(numbers); << "\n";
+    cout << "simple:" << MaxPairwiseProduct(numbers) << "\n";
+    cout << "fast:" << MaxPairwiseProductFast(numbers) << "\n";
     return 0;
 }
