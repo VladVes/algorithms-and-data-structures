33a14fef48252d230e5441465de1f35e79c6d412
VladVes <veselov.workflow@gmail.com>
2019-03-10 22:14:18 +0700

    update greedy money change

--- coursera.org/src/03-greedy_algorithms/change.cpp
+++ coursera.org/src/03-greedy_algorithms/change.cpp
@@ -1,8 +1,25 @@
 #include <iostream>
 
+const int N = 3;
+const int NOMINALS[N] = { 1, 5, 10 };
+
 int get_change(int m) {
-  //write your code here
-  return n;
+  int coinsCounter = 0;
+  if (m == 0) {
+      return coinsCounter;
+  }
+  int rest = m;
+  int currentNominal = N - 1;
+  while (rest != 0) {
+    const int currentRest = rest - NOMINALS[currentNominal]; 
+    if (currentRest >= 0) { 
+        rest = currentRest; 
+        ++coinsCounter;
+    } else { 
+        --currentNominal; 
+    }
+  }
+  return coinsCounter;
 }
 
 int main() {
