a48b121727da75a8c199038569f1f8805aeb5a48
VladVes <veselov.workflow@gmail.com>
2019-03-20 18:11:03 +0700

    update greedy knapsack

--- coursera.org/src/03-greedy_algorithms/fractional_knapsack.cpp
+++ coursera.org/src/03-greedy_algorithms/fractional_knapsack.cpp
@@ -5,22 +5,33 @@ using std::vector;
 
 double get_optimal_value(int capacity, vector<int> weights, vector<int> values) {
   double value = 0.0;
+  std::cout << "function start" << std::endl;
   const int size = weights.size();
-  vector<int> sortedIndexes(size);
-  for (int i = 0; i < size; ++i) {
+  std::cout << "sorted indexex size: " << size << std::endl;
+  vector<int> sortedIndexes;
+  for (int i = 0; i < size; i++) {
      double itemValue = 0.0;
      int currentIndex = i;
-     for (int j = i; j < size - i; ++j) {
+     std::cout << "outer sort cycle i: " << i << " current index: " << currentIndex << std::endl;
+     for (int j = 0; j < size; j++) {
         int currentItemValue = values[j] / weights[j];
         if (itemValue < currentItemValue) { 
-            itemValue = currentItemValue; 
+            itemValue = currentItemValue;
             currentIndex = j;
         }
+	std::cout << "inner sort cycle j: " << j << " item value: " << itemValue << " currentItemValue: " << currentItemValue << std::endl;
      }
+     std::cout << "sorted indexes push " << currentIndex << std::endl;
      sortedIndexes.push_back(currentIndex);
-     std::cout << sortedIndexes[i];
   }
-  int capacityRest = capacity;   
+  std::cout << "result:  " << sortedIndexes.size() << std::endl;
+  for (int x = 0; x < sortedIndexes.size(); x++) {
+     std::cout << "sorted index: " << sortedIndexes[x] << std::endl;
+     std::cout << "value: " << values[sortedIndexes[x]] << std::endl;
+     std::cout << "weights:  " << weights[sortedIndexes[x]] << std::endl;
+  }
+  
+  int capacityRest = capacity;
   while (capacityRest > 0) {
     for (int i = 0; i < size; ++i) {  
         if (capacityRest >= weights[sortedIndexes[i]]) {
@@ -39,14 +50,16 @@ int main() {
   int n;
   int capacity;
   std::cin >> n >> capacity;
+  std::cout << n;
   std::cout << "ready for calculation!" << n << " " << capacity << std::endl;
   vector<int> values(n);
   vector<int> weights(n);
   for (int i = 0; i < n; i++) {
+    std::cout << i << "I" << std::endl;
     std::cin >> values[i] >> weights[i];
-    std::cout << "value: " << values[i] << " weight: " << weights[i] << std::endl;
+    std::cout << "value: " << values[i] << " weight: " << weights[5] << std::endl;
   }
-  std::cout << "data received";
+  std::cout << "data received" << std::endl;
   double optimal_value = get_optimal_value(capacity, weights, values);
 
   std::cout.precision(10);
