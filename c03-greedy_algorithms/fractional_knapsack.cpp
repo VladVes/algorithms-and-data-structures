9da962884e89b7b8b98cff442b67a965db2adf45
VladVes <veselov.workflow@gmail.com>
2019-03-21 23:21:35 +0700

    fix greedy knapsack with comments

--- c03-greedy_algorithms/fractional_knapsack.cpp
+++ c03-greedy_algorithms/fractional_knapsack.cpp
@@ -3,44 +3,68 @@
 
 using std::vector;
 
+void printVector(std::string s, vector<int> v) {
+  std::cout << std::endl;
+  std::cout << "Printing vector - \"" << s << "\": " << std::endl;
+  for (int i = 0; i < v.size(); ++i) {
+      std::cout << "vector[" << i << "] == " << v[i] << std::endl;
+    }
+}
+
 double get_optimal_value(int capacity, vector<int> weights, vector<int> values) {
   double value = 0.0;
   std::cout << "function start" << std::endl;
   const int size = weights.size();
   std::cout << "sorted indexex size: " << size << std::endl;
-  vector<int> sortedIndexes;
-  for (int i = 0; i < size; i++) {
-     double itemValue = 0.0;
-     int currentIndex = i;
-     std::cout << "outer sort cycle i: " << i << " current index: " << currentIndex << std::endl;
-     for (int j = 0; j < size; j++) {
-        int currentItemValue = values[j] / weights[j];
-        if (itemValue < currentItemValue) { 
-            itemValue = currentItemValue;
-            currentIndex = j;
-        }
-	std::cout << "inner sort cycle j: " << j << " item value: " << itemValue << " currentItemValue: " << currentItemValue << std::endl;
-     }
-     std::cout << "sorted indexes push " << currentIndex << std::endl;
-     sortedIndexes.push_back(currentIndex);
-  }
-  std::cout << "result:  " << sortedIndexes.size() << std::endl;
-  for (int x = 0; x < sortedIndexes.size(); x++) {
-     std::cout << "sorted index: " << sortedIndexes[x] << std::endl;
-     std::cout << "value: " << values[sortedIndexes[x]] << std::endl;
-     std::cout << "weights:  " << weights[sortedIndexes[x]] << std::endl;
-  }
+  vector<int> sIndexes(size);
+  vector<int> dIndexes;
   
+  for (int i = 0; i < size; ++i) {
+    sIndexes[i] = i;
+  }
+
+  printVector("source indexes", sIndexes);
+  
+  while (sIndexes.size() > 0) {
+  // int s = size;
+  // while (s != 0) {
+  double itemValue = 0.0;
+  int currentIndex = 0;
+
+    for (int j = 0; j < sIndexes.size(); j++) {
+      double currentItemValue = values[sIndexes[j]] / weights[sIndexes[j]];
+      std::cout << "values[sIndexes[j]] / weights[sIndexes[j]]: " << values[sIndexes[j]] / weights[sIndexes[j]] << std::endl;
+      std::cout << "iter: " << j << std::endl;
+      std::cout << "value: " << values[sIndexes[j]] << " weights: " << weights[sIndexes[j]] << " val per item: " << currentItemValue << std::endl;
+      if (itemValue < currentItemValue) {
+          itemValue = currentItemValue;
+          currentIndex = j;
+          std::cout << "BEST VALUE: " << itemValue << std::endl;
+          std::cout << "INDEX of best val: " << sIndexes[currentIndex] << std::endl;
+      }
+    }
+    dIndexes.push_back(sIndexes[currentIndex]);
+    std::cout << "INDEX PUSHED: " << sIndexes[currentIndex] << " with: ";
+    std::cout << "VAL: " << values[sIndexes[currentIndex]] << " WEIGHT: " << weights[sIndexes[currentIndex]] << std::endl;
+    sIndexes.erase(sIndexes.begin() + currentIndex);
+  }
+  printVector("dest indexes", dIndexes);
+
+  for (int i = 0; i < size; ++i) {
+    std::cout << "VAL: " << values[dIndexes[i]] << " WEIGHT: " << weights[dIndexes[i]] << std::endl;
+  }
+
   int capacityRest = capacity;
   while (capacityRest > 0) {
-    for (int i = 0; i < size; ++i) {  
-        if (capacityRest >= weights[sortedIndexes[i]]) {
-            capacityRest = capacityRest - weights[sortedIndexes[i]];
-            value = value + values[sortedIndexes[i]];
+    for (int i = 0; i < size; ++i) {
+        if (capacityRest >= weights[dIndexes[i]]) {
+            capacityRest = capacityRest - weights[dIndexes[i]];
+            value = value + values[dIndexes[i]];
         } else {
-            double itemValue = values[sortedIndexes[i]] / weights[sortedIndexes[i]];
-            value = value + itemValue * capacityRest;
-        } 
+            double itemValue = values[dIndexes[i]] / weights[dIndexes[i]];
+            value += itemValue * capacityRest;
+            capacityRest = 0;
+        }
      }
   }
   return value;
@@ -50,19 +74,16 @@ int main() {
   int n;
   int capacity;
   std::cin >> n >> capacity;
-  std::cout << n;
-  std::cout << "ready for calculation!" << n << " " << capacity << std::endl;
   vector<int> values(n);
   vector<int> weights(n);
   for (int i = 0; i < n; i++) {
-    std::cout << i << "I" << std::endl;
     std::cin >> values[i] >> weights[i];
-    std::cout << "value: " << values[i] << " weight: " << weights[5] << std::endl;
   }
-  std::cout << "data received" << std::endl;
+  // double result = test();
   double optimal_value = get_optimal_value(capacity, weights, values);
 
   std::cout.precision(10);
   std::cout << optimal_value << std::endl;
   return 0;
+
 }
