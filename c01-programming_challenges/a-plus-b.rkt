c5e2a46c586f3a6ee59ee00b8f88872887ab8c46
VladVes <veselov.workflow@gmail.com>
2022-02-18 13:42:08 +0700

    update sample

--- c01-programming_challenges/a-plus-b.rkt
+++ c01-programming_challenges/a-plus-b.rkt
@@ -1,5 +1,8 @@
 #lang racket
 
+(printf "Enter number for A:")
 (define a (read-line))
+(printf "Enter number for B:")
 (define b (read-line))
+(printf "A + B = ")
 (+ (string->number a) (string->number b))
