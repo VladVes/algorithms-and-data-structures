c260515290c98c2d7661885ca2bc4f6430870bd2
VladVes <veselov.workflow@gmail.com>
2022-02-19 14:10:44 +0700

    update

--- c01-programming_challenges/max-pairwise-product.rkt
+++ c01-programming_challenges/max-pairwise-product.rkt
@@ -1,18 +1,22 @@
 #lang racket
 
 (define (fill-vector v i)
-  ((define vecLen (vector-length v))
-   (cond 
-     [(<= i (- vecLen 1)) 
-      ((printf "Enter number: ") 
-       (vector-set! v i (string->number (read-line)))
-       (fill-vector v (+ i 1)))]
-     [(> i (- vecLen 1)) (v)]
-     [else v]
-    )
-))
+    (define (iter v)
+        (printf "enter number: ")
+        (vector-set! v i (string->number (read-line)))
+        (fill-vector v (+ i 1)))
+    (if (<= i (- (vector-length v) 1)) 
+        (iter v)
+        v))
 
-(printf "Enter array length: ")
-(define vecLength (read-line))
-(define vec (make-vector (string->number vecLength)))
+(define (max-pairwise-product vec)
+    (define vec-len (vector-length vec))
+    (define first-max-index 1)
+    (define second-max-index 2)
+    (* (vector-ref vec first-max-index) (vector-ref vec second-max-index)))
+
+(printf "Enter vector length: ")
+(define vec-len (read-line))
+(define vec (make-vector (string->number vec-len)))
 (fill-vector vec 0)
+(max-pairwise-product vec)
