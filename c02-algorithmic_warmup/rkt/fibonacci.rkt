e2b72370db081c639fb16203d7f3d77c4ec6cfd5
VladVes <veselov.workflow@gmail.com>
2022-02-23 21:47:59 +0700

    update fibonacci

--- c02-algorithmic_warmup/rkt/fibonacci.rkt
+++ c02-algorithmic_warmup/rkt/fibonacci.rkt
@@ -4,6 +4,21 @@
     (if (<= n 1) n
         (+ (fibonacci-naive (- n 1)) (fibonacci-naive (- n 2)))))
 
+(define (fibonacci-fast n)
+    (define (calculate)
+        (define vec (make-vector (+ n 1)))
+        (vector-set! vec 0 0)
+        (vector-set! vec 1 1)
+        (for ([i (inclusive-range 2 n)])
+            (vector-set! vec i (+ (vector-ref vec (- i 1))
+                                  (vector-ref vec (- i 2)))))
+        (vector-ref vec n))
+    (if (< n 2) n
+        (calculate)))
+
 (printf "Enter sequence length: \n")
 (define seq-len (string->number (read-line)))
+(printf "Fibonacci naive: ")
 (fibonacci-naive seq-len)
+(printf "Fibonacci fast: ")
+(fibonacci-fast seq-len)
