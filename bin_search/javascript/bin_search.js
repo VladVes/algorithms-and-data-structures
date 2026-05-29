39b20616d5eb417581847cdd3a02addbaad108c8
VladVes <vccs@inbox.ru>
2026-05-29 13:03:53 +0700

    add js basic bin search

--- bin_search/javascript/bin_search.js
+++ bin_search/javascript/bin_search.js
@@ -4,4 +4,19 @@ function bin_search(list, target) {
     let low = 0;
     let high = list.length - 1
 
+    while (low <= high) {
+        const mid = Math.floor((low + high) / 2)
+        const current = list[mid]
+        if (current == target) {
+            return mid
+        } 
+        if (current < target) {
+            low = mid + 1
+        } else {
+            high = mid - 1
+        }
+    }
+    return null
 }
+
+console.log(bin_search(l, 90))
